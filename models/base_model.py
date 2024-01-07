3d225d1a098d59210ee09fa1e977a2e46e54a1be
ilias <iliaschokairi@gmail.com>
2024-01-07 10:56:32 +0100

    creat instintiation with kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -3,15 +3,21 @@ from uuid import uuid4
 from datetime import datetime
 
 
-
 class BaseModel:
     """BaseModel class"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """instantiation"""
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.update_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.update_at = datetime.now()
 
     def __str__(self):
         return "[BaseModel] ({}) {}".format(self.id, self.__dict__)
@@ -19,7 +25,6 @@ class BaseModel:
     def save(self):
         """update my pulique attribute updated_at with current time updated"""
         self.updated_at = datetime.now()
-
 
     def to_dict(self):
         "Method that return a dictionary of my instances"
