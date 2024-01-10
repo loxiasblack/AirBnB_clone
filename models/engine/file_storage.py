8eae929ad187326c62e14baa786273480f5981e1
ilias <iliaschokairi@gmail.com>
2024-01-10 11:28:25 +0100

    all good

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 import json
-import os
 
 
 class FileStorage:
-    """"""
+    """a class that allow to serialize and
+        desirilize object and save"""
 
     __file_path = "file.json"
     __objects = {}
-    
+
     def all(self):
         """"""
         return FileStorage.__objects
@@ -18,17 +18,15 @@ class FileStorage:
         if obj:
             key = "{}.{}".format(obj.__class__.__name__, obj.id)
             FileStorage.__objects[key] = obj
-            
-
 
     def save(self):
-        """"""
+        """serialize from python to json file"""
         from models.base_model import BaseModel
         with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
-            json.dump({k: v.to_dict() for k, v in FileStorage.__objects.items() if
-                        isinstance(v, BaseModel)}, f)
-            
-    
+            json.dump({k: v.to_dict() for k, v in
+                       FileStorage.__objects.items() if
+                       isinstance(v, BaseModel)}, f)
+
     def reload(self):
         """"""
         from models.base_model import BaseModel
@@ -40,5 +38,4 @@ class FileStorage:
         except FileNotFoundError:
             pass
         except json.decoder.JSONDecodeError:
-        # File is empty or badly formatted, no action needed
             pass
